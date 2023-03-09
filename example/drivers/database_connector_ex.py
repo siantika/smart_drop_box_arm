3ddcd9867dbf7e092e6f99f500d3c9b5441f63d2
I Putu Pawesi Siantika <csiantika@gmail.com>
2023-03-09 13:59:52 +0800

    [Fix] database connector
    Added encoding methods to post, update, and delete methods. Tested in
    local server (works). Refactored database connector example.

--- example/drivers/database_connector_ex.py
+++ example/drivers/database_connector_ex.py
@@ -1,3 +1,13 @@
+'''
+    If server requeires Auth, you need to perform encoding methods.
+    eg: 
+    "
+        set_encode()
+        encode()
+    "
+
+'''
+
 import sys
 sys.path.append('drivers/database_connector')
 
@@ -6,6 +16,9 @@ from database_connector import DatabaseConnector
 # for spacing line
 space = lambda:print()
 splitter = lambda sym, iter :print(f"{sym}" * iter)
+
+# secret
+LOCAL_SECRET = '0534f1025fc5b2da9a41be5951116816bedf30f336b65a8905716eccb800b8c1'
 
 # endpoints
 URL_GET     = 'http://localhost/smart_drop_box/get.php'
@@ -18,7 +31,7 @@ splitter('#',55)
 space()
 # get data from database based on 'id' parameter (OKAY test)
 db = DatabaseConnector(URL_GET)
-result = db.get_data('id','3')
+result = db.get_data('id','12')
 print("********** Get data (TEST OKAY): **********")
 print("* Get data from database [message]:")
 print("  ---->",result[1])
@@ -44,8 +57,18 @@ print("* Get data from database [status_code]:")
 print("  ---->",result[0])
 space()
 splitter('*',55)
+
 # POST data to database in JSON format and return JSON response {'status' and 'new id inserted'}
 db = DatabaseConnector(URL_POST)
+db.set_encode(
+    secret= LOCAL_SECRET,
+    algo='HS256',
+    token_type='Bearer'
+)
+db.encode(
+    {"":""}
+)
+    
 print('Post data to database:')
 print(
     db.post_data(
@@ -53,6 +76,7 @@ print(
     no_resi="9978"
     )
 )
+
 # POST data to database in JSON format and return JSON response (BAD REQUEST test)
 print('Post data to database (bad request):')
 print(
@@ -67,8 +91,16 @@ splitter('*', 55)
 # UPDATE data in database
 # We can't update the same data content, it will return 404 status code (no data found)
 db =  DatabaseConnector(URL_UPDATE)
+db.set_encode(
+    secret= LOCAL_SECRET,
+    algo='HS256',
+    token_type='Bearer'
+)
+db.encode(
+    {"":""}
+)
 
-id_update = 3
+id_update = 12
 print(f'Updated data with id number {id_update}:')
 print(db.update_data('id',str(id_update), name='arduino promini', no_resi='0023'))
 space()
@@ -76,8 +108,16 @@ splitter('*',55)
 
 # DELETE data in database
 db =  DatabaseConnector(URL_DELETE)
+db.set_encode(
+    secret= LOCAL_SECRET,
+    algo='HS256',
+    token_type='Bearer'
+)
+db.encode(
+    {"":""}
+)
 
-id_delete = 11
+id_delete = 12
 print(f'Deleted data with id number {id_delete}:')
 print(db.delete_data('id', str(id_delete)))
 space()
