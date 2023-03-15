cef60dd35ed4bec2aea11708ab0b73aff3d01a0a
I Putu Pawesi Siantika <csiantika@gmail.com>
2023-03-15 11:12:49 +0800

    [Feature] storage thread
    Created and tested storage thread and it's example.
    Added secret key variable to 'config.ini' file.

--- src/main.py
+++ src/main.py
@@ -1,18 +1,23 @@
 import sys
-import json
+import queue
 sys.path.append('applications/storage_thread')
 from storage_thread import StorageThread
 
+q_to_opt      = queue.Queue(5)
+q_from_opt    = queue.Queue(5)
+q_from_server = queue.Queue(5)
+q_to_server   = queue.Queue(5)
+
+# q_from_opt.put(
+#     {
+#         # "method"  : "get",
+#         # "id" : 93
+#         # "name"    : "SHT11",
+#         # "no_resi" : "0502"
+#     }
+# )
+
 str_t = StorageThread()
-
-data = {
-    "method" : "GET",
-    "id"     : 49,
-
-}
-
-ret = str_t.send_request(
-              data
-)
-
-print(ret[0])
+str_t.set_queue(q_from_opt, q_to_opt, q_from_server, q_to_server)
+print(str_t._get_secret())
+#print(q_to_opt.get())
