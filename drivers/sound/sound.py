8a1325c74133de7da2b17c87c17252b963618244
I Putu Pawesi Siantika <csiantika@gmail.com>
2023-03-15 15:25:04 +0800

    [Fix and Refactor]
    Deleted the logger in sound driver. It created ambigous logger. instead,
    we use raise errors when errors occurred. Changed print method to logging
    
    Fix spaces in storage_thread.

--- drivers/sound/sound.py
+++ drivers/sound/sound.py
@@ -34,7 +34,7 @@ import configparser
 import datetime
 import os
 import subprocess
-
+import logging
 
 _SOUND_CONF_FILE_PATH = './conf/config.ini'
 _SOUND_CONF_FILE_PATH_FALSE = './conf/confiasdg.ini' ### only for testing
@@ -42,42 +42,37 @@ _SOUND_CONF_FILE_PATH_FALSE = './conf/confiasdg.ini' ### only for testing
 class Sound:
     def __init__(self):
         self._now = datetime.datetime.now()
-
         self._file = configparser.ConfigParser()
         try:
             self._file.read(_SOUND_CONF_FILE_PATH)
             self._volume = self._file.getfloat('setting', 'volume')
             self._sound_card = self._file.get('setting', 'sound_card')
 
-        except configparser.NoSectionError:
-            if not os.path.exists('./log'):
-                try:
-                    os.makedirs('./log')
-
-                except OSError as e:
-                    print(e)
-
-            with open('./log/log.txt', 'a') as file:
-                file.write("["+self._now.strftime("%Y-%m-%d %H:%M:%S")+"]" + " -->  No config file was found!\n")
+        except configparser.NoOptionError as error_message:
+            raise configparser.Error('volume and sound_card not found in setting section')\
+                  from error_message
+        except configparser.NoSectionError as error_message:
+            raise configparser.Error('setting section not found in configuration file') \
+                from error_message
+        except configparser.Error as error_message:
+            raise configparser.Error(f'Error reading configuration file: {error_message}')
 
     
     def play(self, sound_dir, sound_file_name):
         self._sound_dir = sound_dir
         self._sound_file_name = sound_file_name
-
         subprocess.run(['aplay', str(self._sound_dir + "/" + self._sound_file_name)])
 
     def stop(Self):
         try:                
             subprocess.run(['pid_target=$(pidof aplay)'])
             subprocess.run(['kill -9 $pid_target'])
-        except FileNotFoundError as e:
-            print(e)
+        except FileNotFoundError as error_message:
+            logging.error(error_message)
 
     def volume_control(self, level_volume_persen: int):
         self._convert_from_persen_to_sound_level = level_volume_persen / 100 * 63
         self._convert_from_persen_to_sound_level = int(round(self._convert_from_persen_to_sound_level,0))
-        
         subprocess.run(['amixer', 'set', 'DAC', str(self._convert_from_persen_to_sound_level)])
 
         
