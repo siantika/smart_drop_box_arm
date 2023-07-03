ccd80da774651399c6948cde2e805c23543ad33e
I Putu Pawesi Siantika <csiantika@gmail.com>
2023-07-03 14:05:13 +0800

    [Refactor] Sound and test sound files Initialized an interface for sound driver. Created tests and tested it. Made play-method non-blocking by implementing multiprocessing.
    Added docstrings for both files and performed linting

--- drivers/sound/sound.py
+++ drivers/sound/sound.py
@@ -1,79 +1,142 @@
-'''
-    File           : sound.py
-    Author         : I Putu Pawesi Siantika, S.T.
-    Year           : Feb, 2023
-    Description    : 
-    
-    This code becomes a driver for sound and orange pi zero lts (armbian). It uses aplay and
-    alsamixer packages. It can play sound (.wav only, using aplay in CLI), stop it (kill the process in CLI), and 
-    control the volume (amixer in CLI). It loads config file in './conf/config.ini' (you have to create it manually). You can change the setting there
-    (volume level and sound card). Here is the 'config.ini' content:
-    #############################
-        [setting]
-        sound_card = card 0 
-        volume = 50.2
+"""
+Module: sound driver
+author: I Putu Pawesi Siantika, S.T.
+date  : Feb 2023, refactored in Jul 2023
 
-    #############################
-        sound_card --> you can see the list with "aplay -l" in CLI
-        volume =  min 0 -- max 100% . but it converts to (1 - 63) based on orange pi zero lts
+This module provides classes for simple operation that sound usually does
+(play, stop, and volume control).
+It works only for 'wav' file!
 
-    Tested on orange pi (armbian jammy 22-11.4) and AMD (ubuntu 20.04 , arch linux, FAILED).
+Classes:
+- SoundInterface (abstract base class) = Represents sound interface.
+- Aplay = Implementation for aplay package on linux
+- NonBlockingPlayInterface (abstract base class) = Represents non blocking 
+  method interface for playing sound.
+- NonBlockingAplay  = Implementation for aplay-play nonblocking play sound.
 
-    * Prerequisites *
-    1. 'config.ini' file then put in './conf/config.ini' dir.
-    2. Download or put this library in your working directory project.
-    3. Import it to your project file (eg. main.py)
+"""
 
-    Example code:
-        see: './example/drivers/sound_ex.py' file!
-
-    License: see 'licenses.txt' file in the root of project
-
-'''
-import configparser
-import datetime
+import multiprocessing as mp
 import os
 import subprocess
-import logging
+from abc import ABC, abstractmethod
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
-full_path_config_file = os.path.join(parent_dir, 'conf/config.ini')
-_SOUND_CONF_FILE_PATH_FALSE = './conf/confiasdg.ini' ### only for testing
 
-class Sound:
-    def __init__(self):
-        self._now = datetime.datetime.now()
-        self._file = configparser.ConfigParser()
-        try:
-            self._file.read(full_path_config_file)
-            self._volume = self._file.getfloat('setting', 'volume')
-            self._sound_card = self._file.get('setting', 'sound_card')
+class SoundInterface(ABC):
+    """ Represents abstract base class for simple sound operations"""
+    @abstractmethod
+    def play(self, file_name:str)-> int:
+        """ plays a sound"""
+        pass
 
-        except configparser.NoOptionError as error_message:
-            raise configparser.Error('volume and sound_card not found in setting section')\
-                  from error_message
-        except configparser.NoSectionError as error_message:
-            raise configparser.Error('setting section not found in configuration file') \
-                from error_message
-        except configparser.Error as error_message:
-            raise configparser.Error(f'Error reading configuration file: {error_message}')
+    @abstractmethod
+    def stop(self, pid:int)-> None:
+        """ stops the ongoing sound"""
+        pass
 
-    
-    def play(self, sound_dir, sound_file_name):
-        self._sound_dir = sound_dir
-        self._sound_file_name = sound_file_name
-        subprocess.run(['aplay', os.path.join(self._sound_dir, self._sound_file_name)])
+    @abstractmethod
+    def volume_control(self, level_percent:int) -> None:
+        """ Controls hardware volume"""
+        pass
 
-    def stop(Self):
-        try:                
-            subprocess.run(['pid_target=$(pidof aplay)'])
-            subprocess.run(['kill -9 $pid_target'])
-        except FileNotFoundError as error_message:
-            logging.error(error_message)
 
-    def volume_control(self, level_volume_persen: int):
-        self._convert_from_persen_to_sound_level = level_volume_persen / 100 * 63
-        self._convert_from_persen_to_sound_level = int(round(self._convert_from_persen_to_sound_level,0))
-        subprocess.run(['amixer', 'set', 'DAC', str(self._convert_from_persen_to_sound_level)])
+class Aplay(SoundInterface):
+    """ Implementation for non-blocking play in aplay package"""
 
+    def play(self, file_name:str)-> int:
+        """ 
+            Plays a sound. Only support wav type.
+                args:
+                    file_name (str) : file name of song and its path
+                returns:
+                    pid of aplay executed (int)
+        """
+        non_block = NonBlockingAplay(file_name)
+        pid = non_block.play_nonblocking()
+        return pid
+
+    def stop(self, pid:int) -> None:
+        """
+            Stops ongoing sound by brute-force the process to be killed
+                args:
+                    pid (int) : pid of aplay process that wants to be killed.
+        """
+        if pid != 0:
+            pid_to_str = str(pid)
+            subprocess.run(
+                [
+                'kill',
+                '-9',
+                pid_to_str,
+                ]
+            )
+
+    def volume_control(self, level_percent:int)-> None:
+        """
+            Controls hardware volume level.
+                args:
+                    level_percent (int) : 0 (minimum) - 100 (max) (sound level range) 
+        """
+        convert_from_persen_to_sound_level = level_percent / 100 * 63
+        convert_from_persen_to_sound_level = int(round(convert_from_persen_to_sound_level,0))
+        subprocess.run(['amixer', 'set', 'DAC', str(convert_from_persen_to_sound_level)])
+
+
+class NonBlockingPlayInterface(ABC):
+    """
+        Represents abstract base class for non blocking playing sound method.
+    """
+    def __init__(self, file_name:str) -> None:
+        """ 
+            Constructor checking file song.
+                args:
+                    file_name (str) : full path + file name of sound.
+                raises:
+                    FileNotFoundError : when path contains filename is not exist.
+            """
+        if file_name == "":
+            raise FileNotFoundError("Name of file song must be filled !")
+        self._file_name = file_name
+
+    @abstractmethod
+    def _play(self)->None:
+        """ Represents private play method """
+        pass
+
+    @abstractmethod
+    def play_nonblocking(self)->int:
+        """ Represents play sound in non blocking process """
+        pass
+
+
+class NonBlockingAplay(NonBlockingPlayInterface):
+    """ 
+        Non blocking process is achieved by create a separate task for each 
+        sound file played.    
+    """
+
+    def _play(self)-> None:
+        """ Private method to play sound in aplay package """
+        subprocess.run([
+            'aplay',
+            str(self._file_name),
+            ],)
+
+    def play_nonblocking(self)-> int:
+        """ 
+        Creates separate process to perform non-blocking operation
+            returns:
+                pid of aplay (int): 0 means no aplay is running, otherwise
+                                it returns aplay PID.
+        """
+        thread = mp.Process(target=self._play)
+        thread.start()
+        process_aplay = subprocess.run([
+            'pidof', 
+            'aplay',
+        ], stdout=subprocess.PIPE)
+        aplay_pid = process_aplay.stdout.decode('utf-8')\
+            .strip('\n')
         
+        return 0 if aplay_pid == '' else int(aplay_pid)
--- test/drivers/test_sound/test_sound.py
+++ test/drivers/test_sound/test_sound.py
@@ -1,107 +1,171 @@
-from unittest.mock import patch, call
-import os
-import sys
-import datetime
-sys.path.append('drivers/sound')
-import configparser
-import subprocess
-from sound import Sound
-import shutil
+"""
+Module: test sound driver
+author: I Putu Pawesi Siantika, S.T.
+date  : in July 2023
 
-class TestSound:
+This module performs unit tests for sound driver.
+
+Classes:
+- TestSoundInterfaceInAplay: Test the interface methods in Aplay package 
+- TestSoundPlayAplay: Test play sound method to able in non blocking process,
+                      returns PID, and returns 0 (int) when file sound is not valid/exist. 
+- testStopSoundAplay: Test stop sound at the midle of sound, random duration stop,
+                      and no sound is played.
+- TestVolumeControl: Test volume control in min, average, and max.
+"""
+import time
+import sys
+import os
+import subprocess
+ABS_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
+sys.path.append(os.path.join(ABS_PATH, 'drivers/sound'))
+from sound import Aplay
+import pytest
+
+class TestSoundInterfaceInAplay():
     def set_up(self):
-        global sound
-        sound = Sound()
+        self.sound = Aplay()
+
+    def tear_down(self):
+        while True:     
+            process_aplay = subprocess.run([
+                'pidof', 
+                'aplay',
+            ], stdout=subprocess.PIPE)
+            aplay_pid = process_aplay.stdout.decode('utf-8').strip('\n')
+            if aplay_pid == '':
+                break
+            time.sleep(1)
+
+    def test_sound_interface_in_aplay_should_be_correct(self):
+        self.set_up()
+        assert hasattr(self.sound, 'play')
+        assert hasattr(self.sound, 'stop')
+        assert hasattr(self.sound, 'volume_control')
+
+
+class TestSoundPlayAplay:
+    def set_up(self):
+        self.sound = Aplay()
+
+    def tear_down(self):
+        ''' Make sure sound played until finished each of test case'''
+        while True:     
+            process_aplay = subprocess.run([
+                'pidof', 
+                'aplay',
+            ], stdout=subprocess.PIPE)
+            aplay_pid = process_aplay.stdout.decode('utf-8').strip('\n')
+            if aplay_pid == '':
+                break
+            time.sleep(1)
+
+    def test_sound_should_play_a_song_without_blocking(self):
+        file_name1 = os.path.join(ABS_PATH, 'assets/sounds/ambil_barang.wav') 
+        self.set_up()
+
+        self.sound.play(file_name1)
+
+        process = subprocess.run(
+            [
+                'pidof',
+                'aplay',
+            ], capture_output=True, text= True
+        )
+
+        assert process.stdout.strip('\n') != ''
+        self.tear_down()
+     
+    def test_sound_play_should_return_pid(self):
+        file_name1 = os.path.join(ABS_PATH, 'assets/sounds/ambil_barang.wav') 
+        self.set_up()
+        ret = self.sound.play(file_name1)
+        assert isinstance(ret, int)
+        self.tear_down()
+
+    def test_sound_play_should_return_0_if_no_file_(self):
+        self.set_up()
+        ret = self.sound.play('no_file')
+        assert ret == 0
+        
+
+class TestStopSoundAplay:
+    def set_up(self):
+        self.sound = Aplay()
 
     def tear_down(self):
         pass
 
-    def test_init_process_should_be_correct(self):
-        '''
-            init process should:
-            
-            1. file's path = ./conf/sound_conf.txt
-            2. Check whether conf file is exist
-            3. if conf file is exist, call readFile
-            4. data read should be correct, data are: volume (float) and card (str) : [0,1] 
-        '''
-
+    def test_sound_should_able_stop_song_correctly(self):
+        file_name1 = os.path.join(ABS_PATH, 'assets/sounds/ambil_barang.wav') 
         self.set_up()
+        pid = self.sound.play(file_name1)
+        self.sound.stop(pid)
+        #check pid again
+        process_aplay = subprocess.run([
+                'pidof', 
+                'aplay',
+            ], stdout=subprocess.PIPE)
+        aplay_pid = process_aplay.stdout.decode('utf-8').\
+            strip('\n')
+        assert aplay_pid == ''
 
-        assert isinstance(sound._file, configparser.ConfigParser)        
-        assert sound._volume == 50.2
-        assert type(sound._volume) == float
-        assert sound._sound_card == 'card 0'
-        assert type(sound._sound_card) == str
-
-        self.tear_down()
-         
-    def test_play_sound_should_be_correct(self):
-        '''
-            Create dir : ./assets/sound first
-            Play method has 2 args, first is music's dir, and second is music file name.
-            music file name should be .wav file.
-            song should play once and full duration.
-
-        '''
+    def test_sound_should_able_stop_song_in_the_midle_of_song_correctly(self):
+        file_name1 = os.path.join(ABS_PATH, 'assets/sounds/ambil_barang.wav') 
         self.set_up()
-        self.patcher1 = patch('subprocess.run')
-        self.mock_subproccess_run = self.patcher1.start()
+        pid = self.sound.play(file_name1)
+        # let the song played half of duration(approx)
+        time.sleep(0.5)
+        # kill it
+        self.sound.stop(pid)
+        #check pid again
+        process_aplay = subprocess.run([
+                'pidof', 
+                'aplay',
+            ], stdout=subprocess.PIPE)
+        aplay_pid = process_aplay.stdout.decode('utf-8').\
+            strip('\n')
+        assert aplay_pid == ''
 
-        sound.play('./assets/sounds','sound_taruh_barang.wav')
-
-        assert sound._sound_dir == './assets/sounds'
-        assert sound._sound_file_name == 'sound_taruh_barang.wav'
-        assert os.path.splitext(sound._sound_file_name)[-1] == '.wav'
-
-        ### play a sound
-        self.mock_subproccess_run.assert_called_once_with(['aplay', './assets/sounds/sound_taruh_barang.wav'])
-
-        self.patcher1.stop()
-        self.tear_down()
-
-    def test_stop_sound_should_be_correct(self):
-        '''
-            kill the PID of aplay should success
-            1. knowing the aplay pid
-            2. kill the pid
-        '''
+    def test_sound_should_able_handling_when_song_isnot_played(self):
         self.set_up()
-        self.patcher1 = patch('subprocess.run')
-        self.mock_subproccess_run = self.patcher1.start()
+        self.sound.stop(0) # no song is played
 
-        sound.stop()
+@pytest.mark.skipif(sys.platform != 'armv7l', reason = "Only test on orange pi zero board with armbian os!")
+class TestVolumeControl:
+    ''' Should been tested in orange pi zero directly'''
+    def set_up(self):
+        self.sound = Aplay()
 
-        self.mock_subproccess_run.assert_has_calls(
-            [
-                call(['pid_target=$(pidof aplay)']),
-                call(['kill -9 $pid_target'])
-            ]
-        )
-        self.tear_down()
+    def _help_get_dac(self):
+        command = ['amixer', 'sget', 'DAC']
+        process = subprocess.Popen(command, stdout=subprocess.PIPE)
+        output, _ = process.communicate()
+        output = output.decode('utf-8')
+        # Parse the output to extract the DAC level
+        lines = output.split('\n')
+        for line in lines:
+            if 'Playback' in line and '[dB]' in line:
+                dac_level = line.split('[dB]')[0].strip()
+                return dac_level
 
-
-    def test_volume_control_should_be_correct(self):
-        '''
-            Test DAC level (0-63 MAX).
-            convert from 0 - 100% to 0-63,
-            set DAC level using cmd 'amixer'
-
-        '''
+    def test_volume_control_should_able_set_the_level_to_max(self):
         self.set_up()
-        self.patcher1 = patch('subprocess.run')
-        self.mock_subproccess_run = self.patcher1.start()
+        self.sound.volume_control(100)
+        dac_now =self._help_get_dac()
+        assert dac_now == 63
 
-        sound.volume_control(80)
-        
-        assert sound._convert_from_persen_to_sound_level == 50
-        self.mock_subproccess_run.assert_called_once_with(['amixer', 'set', 'DAC', '50'])
+    def test_volume_control_should_able_set_the_level_to_min(self):
+        self.set_up()
+        self.sound.volume_control(0)
+        dac_now =self._help_get_dac()
+        assert dac_now == 0
 
-        self.patcher1.stop()
-        self.tear_down()
-        
-
- 
+    def test_volume_control_should_able_set_the_level_to_average(self):
+        self.set_up()
+        self.sound.volume_control(50)
+        dac_now =self._help_get_dac()
+        assert dac_now == 31
 
 
         
