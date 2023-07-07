93af2f03c7150ea8615cc4f92c00b869e79beaaa
I Putu Pawesi Siantika <csiantika@gmail.com>
2023-07-07 21:00:01 +0800

    [Refactor] keypad driver and its testcases Made interfaces for it so we can initiate another keypad implementation without break the existing code.

--- drivers/keypad/keypad.py
+++ drivers/keypad/keypad.py
@@ -1,73 +1,112 @@
-'''
-    File           : keypad.py
-    Author         : I Putu Pawesi Siantika, S.T.
-    Year           : Feb, 2023
-    Description    : 
-    
-   This code helps to comunicate between keypad and orange pi. I use serial communication
-   for this protocol between keypad and orange pi. The keypad will send a char (in truth is string) 
-   with special chars "/r/n". So, we need to decode and strip that data and simply return it as str
-   in Python.
+"""
+Module: keypad_driver
+author: I Putu Pawesi Siantika, S.T.
+date  : Feb 2023. Refactored in July 2023.
 
-    * Prerequisites *
-    1. Download or put this library in your working directory project.
-    2. Import it to your project file (eg. main.py)
+This module provides classes for receiving data from keypad.
 
-    Example code:
-        see: './example/keypad_ex.py' file!
+Classes:
+- KeypadInterface (abstract base class): Defines the common interface for keypad method.
+- KeypadUartMatrix (inherits KeypadInterface): Implementation of keypad using keypad matrix with
+                                                uart protocol.
+- KeypadProtocolInterface (abs): Defines the common interface for keypads protocol.
+- KeypadProtocolUart (inherits KeypadProtocolInterface): Implementation of keypad protocol using
+  uart protocol.
 
-    License: see 'licenses.txt' file in the root of project
-
-    ref: https://forum.armbian.com/topic/24562-armbian-orange-pi-read-matrix-keypad-4x4-in-python/
-
-
-'''
- 
+"""
 import os
 import sys
-import serial
 import platform
+from abc import ABC,abstractmethod
+import serial
+from pathlib import Path
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+sys.path.append(os.path.join(parent_dir, 'drivers/mock_wiringpi'))
 
 if platform.machine() == 'armv7l':
     import wiringpi 
     from wiringpi import GPIO
-    
 else:
-    parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
-    sys.path.append(os.path.join(parent_dir, 'drivers/mock_wiringpi'))
     from mock_wiringpi import MockWiringPi, GPIO
     wiringpi = MockWiringPi()
 
 
-class Keypad:
-    def __init__(self,):
-        self.ser = serial.Serial(
-        port='/dev/ttyS1',  # UART1 on Orange Pi
-        baudrate=9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-        )       
+class KeypadInterface(ABC):
+    """ Interface for Keypad"""
+    @abstractmethod
+    def reading_input_char(self)-> str:
+        """ Reads an input char by user"""
+        pass
+
+class KeypadProtocolInterface(ABC):
+    """ Interface for keypad protocol"""
+    @abstractmethod
+    def initialize(self)-> None:
+        """ Initializes a protocol"""
+        pass
+
+
+class KeypadProtocolUart(KeypadProtocolInterface):
+    """ 
+        Implementation of Uart protocol for acessing keypad.
+        Please make sure the keypad supports uart protocol
+        and make sure if the baudrate of SBC and keypad should 
+        be matched.
+    """
+    def __init__(self, port:Path, baudrate:int) -> None:
+        """
+            Constructor of KeypadProtocolUart class. It initialize
+            a serial object from pySerial class.
+                args:
+                    port(Path) : the path where uart used.
+                    baudrate (int) : transfer bit rate.    
+        """
+        self._port = port 
+        self._baudrate = baudrate
     
-    def reading_input_char(self):
-        '''
-            Reading input char from keypad. It uses serial com between keypad and this device
-            (red: orange pi zero lts).
+    def initialize(self) -> None:
+        """
+            Initialize uart protocol using serial library.
+            The configurable args only: port and baudrate.
+                Raises:
+                Inherited from pySerial class:
+                    ValueError : when input of baudrate out of range.
+                    SerialException : when device can't be found or
+                                    can't be configured.
+        """
+        self.uart = serial.Serial(
+        port = self._port,
+        baudrate = self._baudrate,
+        parity = serial.PARITY_NONE,
+        stopbits = serial.STOPBITS_ONE,
+        bytesize = serial.EIGHTBITS,
+        timeout = 1 # secs
+        )
         
+
+class KeypadMatrixUart(KeypadInterface):
+    """
+        Implementation of keypad matrix using uart protocol.
+    """
+    def __init__(self, communication:KeypadProtocolUart):
+        self._communication = communication
+    
+    def reading_input_char(self)-> str:
+        '''
+            Reading input char from keypad. 
+            It uses a serial communication between keypad and SBC.
+                Returns:
+                    char_data (str): single char from keypad.
         '''
         char_data = None
         try:
-            char_data = self.ser.readline().decode('utf-8').strip() 
+            char_data = self._communication.readline()\
+                .decode('utf-8').strip() 
         except UnicodeDecodeError as msg_error:
             print(f"Error keypad: {msg_error}")
         finally:
-            if char_data != None:
+            if char_data is not None:
                 if len(char_data) == 0:   
                     char_data = None
-            return char_data
-    
-    
-
-
-
+        return char_data
--- test/drivers/test_keypad/test_keypad.py
+++ test/drivers/test_keypad/test_keypad.py
@@ -1,138 +1,66 @@
-from unittest.mock import patch, call
-import time
+"""
+Module: test_keypad
+author: I Putu Pawesi Siantika, S.T.
+date  : July 2023
+
+This module performs unit tests for keypad driver.
+
+Classes:
+- TestKeypadMatrixUart : Test behaviour of keypad using
+                         uart protocol
+"""
+
+
 import sys
-
-
+import pytest
+import serial
 sys.path.append('drivers/keypad')
 sys.path.append('drivers/mock_wiringpi')
 
-from keypad import Keypad
+from keypad import *
 
-test_row_pins_array = [2,3,4,5]
-test_column_pins_array = [6,7,8,9]
+TEST_SERIAL_PATH = 'dev/ttyS1'
+TEST_BAUDRATE = 9600
 
-
-class TestKeypad:
-
-    def help_mock_wiringpi_methods(self):
-        self.patcher1 = patch('mock_wiringpi.MockWiringPi.wiringPiSetup')
-        self.patcher2 = patch('mock_wiringpi.MockWiringPi.pinMode')
-        self.patcher3 = patch('mock_wiringpi.MockWiringPi.digitalRead')
-        self.patcher4 = patch('mock_wiringpi.MockWiringPi.digitalWrite')
-        self.patcher5 = patch('mock_wiringpi.MockWiringPi.pullUpDnControl')
-
-        self.mock_wiringpi_wiringPiSetup = self.patcher1.start()
-        self.mock_wiringpi_pinMode = self.patcher2.start()
-        self.mock_wiringpi_digitalRead = self.patcher3.start()
-        self.mock_wiringpi_digitalWrite = self.patcher4.start()
-        self.mock_wiringpi_pullUpDnControl = self.patcher5.start()
-
+class TestKeypadMatrixUart:
+    """
+        Only posible to test the init method. To get comprehensive result,
+        we should test directing to hardware level.
+    """
     def set_up(self):
-        global keypad
-        self.help_mock_wiringpi_methods()
-        keypad = Keypad([2,3,4,5], [6,7,8,9]) ### 1st arg = L || 2nd arg = C
-
-    def tear_down(self):
-        patch.stopall()
-
-
-    def test_init_process_should_be_correct(self):
-        '''
-            init process --> aray pin of L (row) 4 items, aray pin of C (column) 4 items, pin Mode for L
-            is OUTPUT, C is INPUT (pull down).
-        '''
-
-        self.set_up()
-     
-        assert keypad._row_pin_array == test_row_pins_array
-        assert keypad._column_pin_array == test_column_pins_array
-
-        self.mock_wiringpi_wiringPiSetup.assert_called_once()
-        self.mock_wiringpi_pinMode.assert_has_calls(
-            [
-                call(2,1), ### output mode
-                call(3,1),
-                call(4,1),
-                call(5,1),   
-
-                call(6,0), ### input mode
-                call(7,0),
-                call(8,0),
-                call(9,0),                              
-            ]
-        )
-
-        self.mock_wiringpi_pullUpDnControl.assert_has_calls(
-            [
-                call(6,1), ### input mode pull down (1) see mock_wiringpi lib !
-                call(7,1),
-                call(8,1),
-                call(9,1),                              
-            ]
-
-        )
-
-        self.tear_down()
-
-    def test_init_proccess_should_handle_lenth_arrays_pin_correctly(self):
-        '''
-            the length of each array should not exceeding or least than 4.
-        '''
-
-        ### exceeding 4 items should raise an ValueError
-        self.help_mock_wiringpi_methods()
-        try:
-            keypad = Keypad([2,2], [4,8,5,4,8,6,5,10,7])
-        except ValueError as ve:
-            assert str(ve) == 'the length of each arrays items should be 4 items'
-        else:
-            assert False
-
-        self.tear_down()
-
-
-
-    '''
-    each keypad will be tested with hardware, dificukt to construct the tests since it paths many.
+        uart_protocol = KeypadProtocolUart('dev/ttyS1', 9600)
+        self.keypad = KeypadMatrixUart(uart_protocol)
     
-    '''
-    def test_read_line_should_be_executed_correctly(self):
-        '''
-            just verify the methods called exactly
-        '''
+    def test_init_should_inisiate_keypadprotocoluart_class(self):
         self.set_up()
-        keypad._read_line(1, ['1', '2', '3', 'A'])
+        isinstance(self.keypad._communication, KeypadProtocolUart)
 
-        self.mock_wiringpi_digitalWrite.assert_has_calls(
-            [
-                call(1,1), # line 1 = HIGH
-                call(1,0) # line 1 = LOW
-            ]
+
+class TestKeypadProtocolUart:
+    def set_up(self):
+        self.uart_protocol = KeypadProtocolUart(
+            TEST_SERIAL_PATH,
+            TEST_BAUDRATE,
         )
 
-    # @patch('time.sleep')
-    # @patch('keypad.Keypad._read_line')
-    # def test_read_input_char_should_put_correct_row_pins_array(self, mock__read_line, mock_time_sleep):
-    #     '''
-    #         Note: please comment the while true in 'keypad.py' to run this test. If in production code,
-    #             please comment this test case!.
-                
-    #     '''
-    #     self.set_up()
-      
-    #     keypad.reading_input_char()
+    def test_attributes_should_be_correct(self):
+        self.set_up()
+        hasattr(self.uart_protocol, '_port')
+        hasattr(self.uart_protocol, '_baudrate')
 
-    #     mock__read_line.assert_has_calls(
-    #         [
-    #             call(test_row_pins_array [0], ['1', '4', '7', '*']),
-    #             call(test_row_pins_array [1], ['2', '5', '8', '0']),
-    #             call(test_row_pins_array [2], ['3', '6', '9', '#']),
-    #             call(test_row_pins_array [3], ['A', 'B', 'C', 'D'])
-    #         ]
-    #     )
-    #     mock_time_sleep.assert_called_once_with(0.2)
+    def test_attributes_should_be_handle_correct_input(self):
+        self.set_up()
+        assert self.uart_protocol._port == TEST_SERIAL_PATH
+        assert self.uart_protocol._baudrate == TEST_BAUDRATE
 
-    #     self.tear_down()
+
+    def test_should_raise_error_when_serial_lib_received_wrong_params(self):
+        """ When args are not inputted correctly, it should raise an Serial
+            Error (inherited from pySerial class)"""
+        
+        with pytest.raises(serial.SerialException):
+            self.set_up()
+            self.uart_protocol.initialize()
         
 
  
