2b101b29576150c2206ef013fc324efa5b66fd4a
I Putu Pawesi Siantika, S.T <pawesisiantika98@gmail.com>
2023-07-25 00:18:37 +0800

    [Added] test periph opt Only works when PeriperalOpeartions class commenteds

--- applications/peripheral_operations/peripheral_operations.py
+++ applications/peripheral_operations/peripheral_operations.py
@@ -1,152 +1,123 @@
 """
-    File           : peripheral_operations.py
+    File           : peripheral operations
     Author         : I Putu Pawesi Siantika, S.T.
-    Year           : Mar, 2023
+    Year           : July, 2023
     Description    : 
 
         Operations of peripherals (camera, weight sensor, GPIOs, sound, and keypad).
         For weight sensor, it is not in gram unit. I found it in other people repo(check
         hx711.py in driver folder)
 
-    License: see 'licenses.txt' file in the root of project
 """
 
 import configparser
 import os
+from pathlib import Path
 import sys
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
-
 sys.path.append(os.path.join(parent_dir, 'drivers/keypad'))
 sys.path.append(os.path.join(parent_dir, 'drivers/weight'))
 sys.path.append(os.path.join(parent_dir, 'drivers/door'))
-sys.path.append(os.path.join(parent_dir, 'drivers/usb_camera'))
+sys.path.append(os.path.join(parent_dir, 'drivers/camera'))
 sys.path.append(os.path.join(parent_dir, 'drivers/sound'))
 sys.path.append(os.path.join(parent_dir, 'utils'))
 
-#Should put here
-from usb_camera import UsbCamera
-from sound import Sound
-from weight import Hx711
-from door import Door
-from keypad import Keypad
+config_dir = os.path.join(parent_dir, 'conf/config.ini')
+# Should put here due to local DIY libs
 from pins_config import PinsConfig
+from keypad import KeypadMatrixUart
+from door import GpioDoor, GpioType
+from weight import HX711Driver
+from sound import Aplay
+from camera import UsbCamera, UsbCameraSetting, CameraResolution
 
+# Constants 
+# Hx711 driver
 REFERENCE_UNIT_WEIGHT = 142  # Calibration unit for weight sensor
-SAMPLES = 9  #  frequency of sampling weight sensor data
 
-full_path_config_file = os.path.join(parent_dir, 'conf/config.ini')
+# Utility functions
+def read_config_file(config_file_path: Path, section: str, param: str) -> str:
+    '''
+    Reads configuration file in 'config.ini'.
+    Args:
+        section (str): The section in the config file.
+        param (str): The parameter in the config file
+    Returns:
+        value of intended param.
+    Raises:
+        configparser.Error: If there is an error reading the 
+                                configuration file 
+    '''
+    parser_secret = configparser.ConfigParser()
+    try:
+        parser_secret.read(config_file_path)
+        parsed_config_data = parser_secret.get(section, param)
+    except Exception as error:
+        raise configparser.Error(f'Error reading configuration file: {error}')
+    return parsed_config_data
+
+
+class PeripheralsSetupCreation:
+    """ Factory creator for peripherals setup"""
+    @staticmethod
+    def create_camera_setup():
+        """ Camera setup 
+            It uses 640 x 480 resolution.
+            camera's hardware address and directory of
+            saved photo are configured in config file
+        """
+        camera_setting = UsbCameraSetting()
+        camera_setting.res = CameraResolution._640x480
+        camera_hw_addr = read_config_file(config_dir,
+                                          'camera', 'hw_address')
+        usb_camera = UsbCamera(camera_hw_addr, camera_setting)
+        usb_camera.dir = read_config_file(config_dir,
+                                          'camera', 'dir_to_save_photo')
+        return usb_camera
+
+    @staticmethod
+    def create_sound_setup():
+        """ Sound setup using Aplay media player"""
+        return Aplay()
+
+    @staticmethod
+    def create_weight_setup():
+        """
+            Weight sensor setup
+            It uses HX711 driver and 142 for ref unit
+            (check weight driver and example).
+            Gpio pins are configured in ConfigPins
+        """
+        weight = HX711Driver(PinsConfig.PD_OUT_WEIGHT,
+                             PinsConfig.SCK_WEIGHT)
+        weight.set_reference_unit(REFERENCE_UNIT_WEIGHT)
+        weight.calibrate()
+        return weight
+
+    @staticmethod
+    def create_door_setup():
+        """ Gpio door setup with active low GPIO
+            Gpio pins are configured in PinsConfig
+        """
+        return GpioDoor(PinsConfig.GPIO_DOOR_LOCK,
+                        PinsConfig.GPIO_DOOR_SENSE,
+                        GpioType.ACTIVE_LOW)
+
+    @staticmethod
+    def create_keypad_setup():
+        """ Keypad setup using uart protocol and 9600 baudrate
+            Serial port is configured in config file
+        """
+        keypad_port = read_config_file(config_dir, 'keypad',
+                                       'port')
+        return KeypadMatrixUart(keypad_port, 9600)
 
 
 class PeripheralOperations:
-    def __init__(self) -> None:
-        self.pin_dout_weight = ''
-        self.pin_sck_weight = ''
-        self.pin_door_lock = ''
-        self.pin_door_sense = ''
-        self.dir_saved_photo = ''
-        self.hw_addr_usb_camera = ''
-        self.sound = ''
-        self.sound_files_dir = ''
-
-        self.keypad = None
-        self.weight = None
-        self.door = None
-        self.camera = None
-
-    def _set_weight_pins(self, pd_out, sck)-> None:
-        self.pin_dout_weight = pd_out
-        self.pin_sck_weight = sck
-
-    def _set_door_pins(self, door_lock, door_sense)-> None:
-        self.pin_door_lock = door_lock
-        self.pin_door_sense = door_sense
-
-    def _get_data_from_config_file(self, section: str, var: str) -> str:
-        parser_data = configparser.ConfigParser()
-        parser_data.read(full_path_config_file)
-        data = parser_data.get(section, var)
-        return data
-
-    def _set_dir_saved_photo(self)-> None:
-        # get from config.ini
-        relative_photos_folder = self._get_data_from_config_file(
-            'usb_camera', 'photo_dir')
-        full_path_photos_dir = os.path.join(parent_dir, relative_photos_folder)
-        self.dir_saved_photo = full_path_photos_dir
-
-    def _set_hw_usb_camera(self)-> None:
-        parsed_config = self._get_data_from_config_file(
-            'usb_camera', 'hw_addr')
-        self.hw_addr_usb_camera = parsed_config
-
-    def _get_dir_sound_files(self)-> None:
-        relative_sound_folder = self._get_data_from_config_file(
-            'dir', 'files_sound')
-        self.sound_files_dir = os.path.join(parent_dir, relative_sound_folder)
-
-    def play_sound(self, file_name: str)-> None:
-        self.sound.play(self.sound_files_dir, file_name)
-
-    def set_all_pins_periphs(self)-> None:
-        pins = PinsConfig()
-        self._set_door_pins(pins.GPIO_DOOR_LOCK, pins.GPIO_DOOR_SENSE)
-        self._set_weight_pins(pins.PD_OUT_WEIGHT, pins.SCK_WEIGHT)
-
-    def init_weight(self)-> None:
-        self.weight.set_reading_format("MSB", "MSB")
-        self.weight.set_reference_unit(REFERENCE_UNIT_WEIGHT)
-        self.weight.reset()
-        self.weight.tare()
-
-    def init_all(self)-> None:
-        self.set_all_pins_periphs()
-        self._set_hw_usb_camera()
-        self.keypad = Keypad()
-        self.weight = Hx711(self.pin_dout_weight,
-                            self.pin_sck_weight)
-        self.door = Door(self.pin_door_lock,
-                         self.pin_door_sense)
-        self.camera = UsbCamera(self.hw_addr_usb_camera)
-        self.sound = Sound()
-        self.init_weight()
-        self._get_dir_sound_files()
-        self._set_dir_saved_photo()
-        self.camera.set_dir_saved_photo(self.dir_saved_photo)
-
-    def read_input_keypad(self)->str:
-        read_char = self.keypad.reading_input_char()
-        return read_char
-
-    def lock_door(self)-> None:
-        self.door.lock_door()
-
-    def unlock_door(self)-> None:
-        self.door.unlock_door()
-
-    def sense_door(self)->bool:
-        door_state = self.door.sense_door_state()
-        return door_state
-
-    def get_weight(self)->float:
-        weight_val = self.weight.get_weight(SAMPLES)
-        # Ease us to read the weight value
-        rounded_weight_val = round(weight_val, 1)
-        return rounded_weight_val
-
-    def set_power_down_weight(self)-> None:
-        self.weight.power_down()
-
-    def set_power_up_weight(self)-> None:
-        self.weight.power_up()
-
-    def capture_photo(self)-> int:
-        return self.camera.capture_photo()
-
-    def delete_photo(self)-> None:
-        # Actually, we delete all photos in assets/photos folder
-        self.camera.delete_all_photos_in_folder()
-
-    def get_photo_name(self) -> list:
-        file_photo_name = self.camera.get_photo_name()
-        return file_photo_name
+    """ Grouping peripherals """
+    camera = PeripheralsSetupCreation.create_camera_setup()
+    sound = PeripheralsSetupCreation.create_sound_setup()
+    weight = PeripheralsSetupCreation.create_weight_setup()
+    door = PeripheralsSetupCreation.create_door_setup()
+    keypad = PeripheralsSetupCreation.create_keypad_setup()
