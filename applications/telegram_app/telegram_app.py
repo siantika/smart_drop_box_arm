fa3744693815e7465f4c98b98a4fff3ea73407a6
I Putu Pawesi Siantika, S.T <pawesisiantika98@gmail.com>
2023-07-25 22:11:57 +0800

    [Refactor] Telegram app and it's Test cases Added docstrings and fixed test cases

--- applications/telegram_app/telegram_app.py
+++ applications/telegram_app/telegram_app.py
@@ -1,14 +1,12 @@
 """
-    File           : telegram_app.py
+    File           : Telgram app
     Author         : I Putu Pawesi Siantika, S.T.
     Year           : Mei, 2023
     Description    : 
 
-        Currently: Send notification with courier's photo to telegram bot using telegram API.
-        The bot already created using BotFather in telegram. All bot's credentials are put in 
-        config file.
+    This file is intended to perform telegram-functionality,
+    such as, send data (text and photo ) to telegram-bot.
 
-    License: see 'licenses.txt' file in the root of project
 """
 import configparser
 import datetime
@@ -19,30 +17,41 @@ import sys
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 full_path_config_file = os.path.join(parent_dir, 'conf/config.ini')
 sys.path.append(os.path.join(parent_dir, 'utils'))
-
 # should put here!
 import log
+# Time out for sending request to telegram server
+# in secs
+REQUEST_TIME_OUT = 10 
+
+# Utility functions
+def read_config_file(option:str)-> str:
+    """ Read intended data from config file """
+    file = configparser.ConfigParser()
+    file.read(full_path_config_file)
+    raw_data =  file.get('telegram', str(option))
+    return raw_data
+
 
 class TelegramApp:
+    """ Application for using telegram API throught 
+        request HTTP. 
+        By now, It only can send a notification (text and photo)
+        to registered telegram-bot.
+    """
     def __init__(self) -> None:
-        self.bot_token = self._read_config_file('bot_token')
-        self.chat_id = self._read_config_file('chat_id')
-        base_api_url = self._read_config_file('api_url')
+        """ Please write the telegram params inside config file 
+        in section 'telegram' """
+        self.bot_token = read_config_file('bot_token')
+        self.chat_id = read_config_file('chat_id')
+        base_api_url = read_config_file('api_url')
         relative_url = f"bot{self.bot_token}/"
         self.api_url = base_api_url + relative_url + "sendPhoto"
 
-    def _read_config_file(self, option:str)-> str:
-        file = configparser.ConfigParser()
-        file.read(full_path_config_file)
-        raw_data =  file.get('telegram', str(option))
-        return raw_data
-    
     def _write_caption_report(self, raw_data_caption:dict):
         '''
-            caption's format (weird indentation) affects the message's content display on phone.
-            the rule for writing text in telegram message (ref: https://core.telegram.org/bots/api 
-            in formatting section)
-
+            Caption's format (weird indentation) affects the message's content display on phone.
+            The rule for writing text in telegram message is in this ref: https://core.telegram.org/bots/api 
+            (see formatting section)
         '''
         time_now = datetime.datetime.now()
         time_now_strf = time_now.strftime("%Y\\-%m\\-%d %H:%M:%S")
@@ -55,12 +64,15 @@ class TelegramApp:
     
     def send_notification(self, raw_caption:dict, bin_photo)-> int:
         '''
-            Call this function to send notification to telegram bot.
-            
-            Params:
-                raw_caption (dict)  : data contained item description (name, no resi, date ordered)
-                                      stored in RAM.
-                bin_photo           : courier's photo in bin format.
+            Sends notification to registered telegram bot.
+            Log the error message if error occurs.
+            Args:
+                raw_caption (dict)  : Description (name, no resi, and
+                                      date ordered) will be displayed belos
+                                      the photo /image.
+                bin_photo           : Photo data in binary format.
+            Returns:
+                Http status code (int)
         '''
         caption = self._write_caption_report(raw_caption)
         try:
@@ -68,7 +80,7 @@ class TelegramApp:
                              data = {"chat_id" : self.chat_id, 
                                     "caption" : caption, 
                                     "parse_mode" : "MarkdownV2", 
-                                    }, files = {"photo" : bin_photo}, timeout= 30)
+                                    }, files = {"photo" : bin_photo}, timeout= REQUEST_TIME_OUT)
         except Exception as e:
             log.logger.exception(str(e))
         else:
--- test/applications/test_telegram_app/test_telegram_app.py
+++ test/applications/test_telegram_app/test_telegram_app.py
@@ -1,12 +1,15 @@
 import sys
+from unittest.mock import patch
+
+import requests
 sys.path.append('applications/telegram_app')
 from telegram_app import TelegramApp
 
 class TestTelegramApp:
     def test_telegram_app_should_has_attributes_correctly(self):
         """
-            This constructor will receives 2 data, first is caption data
-            (item_name, no_resi, and date_ordered) and second is photo file
+            This constructor will receive 2 data, the first is caption data
+            (item_name, no_resi, and date_ordered) and the second is photo file
             in binary format.
         """
 
@@ -16,29 +19,20 @@ class TestTelegramApp:
         assert hasattr(telegram, 'chat_id')
         assert hasattr(telegram, 'api_url')
 
-    def test_bot_token_should_be_correct(self):
-        telegram = TelegramApp()
-
-        assert telegram.bot_token == "6056340407:AAEW9tR5pg7v22B23qMhLi3TVlY5eKly9qg"
-
-    def test_chat_id_should_be_correct(self):
-        telegram = TelegramApp()
-        assert telegram.chat_id == '679839756'
-
-    def test_api_url_should_be_correct(self):
-        telegram = TelegramApp()
-        assert telegram.api_url == 'https://api.telegram.org/bot6056340407:AAEW9tR5pg7v22B23qMhLi3TVlY5eKly9qg/sendPhoto'
-
-
     def test_send_post_request_to_telegram_api_should_be_correct(self):
-        test_raw_caption = {'name': 'spidol',
-                    'no_resi': '0025',
-                    'date_ordered': '2023-03-01 10:18:45',
-                    }
-        with open('test/applications/test_telegram_app/test.jpg', 'rb') as f:
-            test_bin_photo = f.read()
-
-        telegram = TelegramApp()
-        return_code = telegram.send_notification(test_raw_caption, test_bin_photo)
-        assert return_code == 200
+        """ Should invoke requests.post method once """
+        with patch.object(requests, 'post') as mock_req_post:
+            mock_req_post.return_value.status_code = None
+            test_raw_caption = {'name': 'spidol',
+                        'no_resi': '0025',
+                        'date_ordered': '2023-03-01 10:18:45',
+                        }
+            with open('test/applications/test_telegram_app/test.jpg', 'rb') as f:
+                test_bin_photo = f.read()
+            telegram = TelegramApp()
+            return_code = telegram.send_notification(test_raw_caption, 
+                                                     test_bin_photo)
+            mock_req_post.assert_called_once()
+            
+            
 
