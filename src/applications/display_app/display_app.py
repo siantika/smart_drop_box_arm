2903371c488591fc30d4806f04db43fe21a11899
I Putu Pawesi Siantika, S.T <pawesisiantika98@gmail.com>
2023-08-04 16:34:46 +0800

    [Change] source code folders to src

---
+++ src/applications/display_app/display_app.py
@@ -0,0 +1,144 @@
+'''
+    File           : Display app
+    Author         : I Putu Pawesi Siantika, S.T.
+    Year           : Mar, 2023
+    Description    : 
+    
+        This code helps for creating a thread for display app. It will print data
+    if queue_data_read attribute is exist with correct data ({'cmd': DisplayMode.NORMAL, 
+    'payload':[firstline_data, secondline_data]}) else it will print the latest data 
+    obtained from queue_data_read attribute.
+        Attributes:
+            queue_data_read (mp.Queue)  : Queue data for reading payload
+                                          from other thread.
+                                          Queue data content is a dict objec.
+                                          eg. {'cmd': 'normal', 
+                                          'payload': [firstline_data, secondline_data]}
+            _Display                     : Display object
+
+    NOTE: This code is intended for multiprocessing style. 
+          Here is the code should be put in the main code (thread creation):
+        """
+            def thread_lcd_func():
+                lcd_t = LcdThread(
+                    args
+                )
+                lcd_t.run()
+        """
+
+    Dependencies:
+        + lcd.py --> driver for LCD 16x2 i2c (Now)
+
+'''
+import multiprocessing as mp
+import os
+import sys
+from enum import Enum, auto
+
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
+sys.path.append(os.path.join(parent_dir,'src/drivers/lcd'))
+sys.path.append(os.path.join(parent_dir,'utils'))
+
+# Using LCD 16 x 2 with I2C protocol
+from lcd import LcdI2C
+from Interfaces import AppInterface
+
+# Make sure your LCD hardware address and spi's bus are correct !
+DEV_ADDR = 0x27
+DEV_BUS = 0
+
+
+class DisplayMode(Enum):
+    """ Enumeration of Display mode.
+    It affects the way of Display printing data.
+    (running text, normal, etc)
+        Currently, it only has normal mode.
+    """
+    NORMAL = auto()
+
+
+class DisplayApp(AppInterface):
+    '''
+        This class is used for creating a display app.
+            Attribs:
+            _queue_data_read (mp.Queue) : Variable that stored data from
+                                          other queue (set queue)
+            _display (LcdI2C) : Lcd Object : Performing lcd basic operations
+                                         with I2C protocol
+    '''
+    def __init__(self)-> None:
+        """ Initiates queue data and display object 
+            By now, we use lcd 16 x 2 i2c for display.
+        """
+        self._queue_data_read = mp.Queue()
+        self._display = LcdI2C(DEV_ADDR, DEV_BUS)
+        self._display.init_lcd()
+
+    def _read_queue_data(self)-> mp.Queue:
+        '''
+            Read queue data from shared resource. 
+            It uses block mechanism, so this method
+            will block the next process until it fed by data from other 
+            thread.
+            By doing this, we saving the CPU usage.
+
+            Returns:
+                payload data from other thread. Data type of queue data
+                is a dict.
+
+        '''
+        return self._queue_data_read.get()
+
+    def set_queue_data(self, queue_data :mp.Queue):
+        '''
+            Set queue data from other thread.
+            User should invoke this function.
+        '''
+        self._queue_data_read = queue_data
+
+    def _parse_dict_data(self, data: dict)-> tuple:
+        '''
+            Parsing dictionary data becoming 2 items (tuple).
+            first data in dict contains command for LCD, the other one 
+            contains payload (data to be displayed on lcd)
+            Raises:
+                ValueError  : if contents in payload are not string.
+            
+            Returns:
+                cmd and text data in tuple. Text data contains list 
+                of string data.
+        '''
+        cmd  = data['cmd']
+        data_text = data['payload']
+        # Payload content should be string type
+        if not all(isinstance(item, str) for item in data_text):
+            raise ValueError("Payload contents should be string!")
+        return cmd, data_text
+
+    def _normal_print(self, first_line:str, second_line:str)-> None:
+        """ Just print data to LCD (no running text, etc) """
+        self._display.clear_lcd()
+        self._display.write_text(0, first_line)
+        self._display.write_text(1, second_line)
+
+    def _print_data(self)-> None:
+        '''
+            Controls behaviours of lcd task.
+            It reads data from shared queue. While no data in 
+            shared queue, it stays in read queue data function
+            (Not continue to next operation)      
+        '''
+        # Read queue data from control thread
+        queue_data = self._read_queue_data()
+        cmd, display_data = self._parse_dict_data(queue_data)
+        mode = DisplayMode[cmd]
+        if mode is DisplayMode.NORMAL:
+            self._normal_print(display_data[0], display_data[1])            
+
+    def run(self)->None:
+        '''
+            Driver method for running the task in infinite loop.
+        '''
+        while True:
+            self._print_data()
+    
---
+++ src/drivers/camera/camera.py
@@ -0,0 +1,159 @@
+"""
+Module: camera_module
+author: I Putu Pawesi Siantika, S.T.
+date  : July 2023
+
+This module provides classes for capturing and managing photos using a camera.
+
+Classes:
+- Camera (abstract base class): Defines the common interface for cameras.
+- UsbCamera (inherits Camera): Implementation of a USB camera (GEMBIRD brand).
+- UsbCameraSetting: Settings for a USB camera.
+- CameraResolution (inherits Enum) : Enumeration of camera resolutions
+
+"""
+from abc import ABC, abstractmethod
+from datetime import datetime
+import subprocess
+import os
+from enum import Enum
+
+abs_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../.."))
+
+
+class CameraResolution(Enum):
+    """ Eumeration of available camera resolutions"""
+    _640x480 = '640x480'
+    _1280x720 = '1280x720'
+    _1920x1080 = '1920x1080'
+
+
+class Camera(ABC):
+    """Abstract base class for a camera."""
+    dir: str = None
+
+    @property
+    @abstractmethod
+    def photo_directory(self) -> str:
+        """Get the directory where photos are saved."""
+        pass
+
+    @photo_directory.setter
+    @abstractmethod
+    def photo_directory(self, directory: str) -> None:
+        """Set the directory where photos are saved."""
+        pass
+
+    @abstractmethod
+    def generate_file_name(self)-> str:
+        '''Generate file name for a single photo'''
+        pass
+
+    @abstractmethod
+    def capture_photo(self) -> str:
+        """Capture a photo using the camera."""
+        pass
+
+    @abstractmethod
+    def get_all_photos_file_name(self) -> list:
+        """Get the file names of all saved photos in directory."""
+        pass
+
+    @abstractmethod
+    def delete_photo(self, file_name: str) -> None:
+        """Delete a specific photo by file name."""
+        pass
+
+    @abstractmethod
+    def delete_all_photos(self) -> None:
+        """Delete all photos in the photo directory."""
+        pass
+
+
+class UsbCameraSetting:
+    """Setting for a USB camera."""
+
+    res: str = None
+
+    @property
+    def resolution(self):
+        """Get the resolution setting for the USB camera."""
+        return self.res
+
+    @resolution.setter
+    def resolution(self, resolution:CameraResolution):
+        """Set the resolution setting for the USB camera."""
+        self.res = resolution.value
+
+
+class UsbCamera(Camera):
+    """Implementation of a USB camera."""
+
+    def __init__(self, hw_address, setting: UsbCameraSetting)-> None:
+        """
+        Initialize a USB camera instance.
+
+        Args:
+            hw_address: The hardware address of the USB camera.
+            setting: The setting object for the USB camera.
+        """
+        self._hw_addr = str(hw_address)
+        self._setting = setting
+        self.dir = None
+
+    @property
+    def photo_directory(self)->str:
+        """Get the directory where photos are saved."""
+        if self.dir is None:
+            raise TypeError('Photo directory should be filled!')
+        return self.dir
+
+    @photo_directory.setter
+    def photo_directory(self, directory)-> None:
+        """Set the directory where photos are saved."""
+        self.dir = directory
+
+    def generate_file_name(self) -> str:
+        """Generate date-time format for file name.""" 
+        return datetime.now().strftime('%Y-%m-%d_%H-%M-%S.jpg')
+
+    def capture_photo(self)-> str:
+        """Capture a photo using the USB camera.
+           Return: success operation = 0,
+                   failed operation  = 1
+                   (int),
+                   dir_saved_photo (relative to the parrent dir)
+        """
+        # create an absolute path for photo file
+        dir_saved_photo = os.path.join(self.dir, self.generate_file_name())
+        ret_opt = subprocess.run([
+            'fswebcam',                    # fswebcam package for Linux
+            '-d',                          # camera device address
+            self._hw_addr,
+            '-r',                          # photo resolution
+            self._setting.resolution,
+            '--save',
+            dir_saved_photo
+        ],capture_output=True, text=True, check=True)
+
+        #add successful operation checking
+        return (1, dir_saved_photo) if 'stat: No such file or directory' in ret_opt.stderr \
+            else (0, dir_saved_photo)
+    
+    def get_all_photos_file_name(self)-> list:
+        """Get the file names of all saved photos."""
+        full_path_photo = os.path.join(abs_path, self.dir)
+        files = os.listdir(full_path_photo)
+        return files
+    
+    def delete_all_photos(self)-> None:
+        """Delete all photos in the photo directory folder."""
+        full_path_photo = os.path.join(abs_path, self.dir)
+        files = os.listdir(full_path_photo)
+        for file in files:
+            os.remove(os.path.join(full_path_photo, file))
+
+    def delete_photo(self, file_name)-> None:
+        """Delete a specific photo base on file name."""
+        full_path_file = os.path.join(self.dir, file_name)
+        os.remove(full_path_file)
